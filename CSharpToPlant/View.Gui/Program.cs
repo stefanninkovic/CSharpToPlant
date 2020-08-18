324abbf75b4ae3b5325fe8757f0953506f95db4a
Stefan Ninkovic <stefannninkovic@gmail.com>
2020-08-18 21:43:59 +0200

    CHANGE: Created Solution

---
+++ CSharpToPlant/View.Gui/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ninkovic.Stefan.CSharpToPlant.View.Gui
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
