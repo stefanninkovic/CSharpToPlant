cfe7c16a2b80e87736a22481feaa1382ce6e37b2
Stefan Ninkovic <stefannninkovic@gmail.com>
2020-08-25 23:23:12 +0200

    CHANGE: Implemented logic for writing down into a file

---
+++ src/Business.File/FileWriter.cs
@@ -0,0 +1,15 @@
+﻿namespace Ninkovic.Stefan.CSharpToPlant.Business.File
+{
+    public class FileWriter
+    {
+        public bool CreateFile(string path, string text)
+        {
+            if (System.IO.File.Exists(path))
+                return false;
+
+            System.IO.File.WriteAllText(path, text);
+
+            return System.IO.File.Exists(path);
+        }
+    }
+}
