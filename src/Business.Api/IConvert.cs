4e6f4ebf37976159515d56825e147b6dc82d420f
Stefan Ninkovic <stefannninkovic@gmail.com>
2020-08-25 22:05:10 +0200

    CHANGE: Added IConvert interface for all converter-classes

---
+++ src/Business.Api/IConvert.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Ninkovic.Stefan.CSharpToPlant.Business.Api
+{
+    public interface IConvert
+    {
+        ICollection<DateTime> Convert(ICollection<Assembly> assemblies);
+    }
+}
